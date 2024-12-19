f8b92e90a93d4c07f98c28cd1ce8d1fd4f4a5961
muchieva <muchina.ieva@gmail.com>
2024-12-19 14:15:42 +0200

    selecting files to read; connecting to one dataframe; visualisation; miimal changes

--- main.py
+++ main.py
@@ -8,18 +8,27 @@ btc = pd.read_csv('files/bitcoin.csv', index_col=0, parse_dates=True)
 print(btc)
 bnb = pd.read_csv('files/BNB.csv', index_col=0, parse_dates=True)
 print(bnb)
-xrp = pd.read_csv('files/xrp.csv', index_col=0, parse_dates=True)
-print(xrp)
+eth = pd.read_csv('files/ethereum.csv', index_col=0, parse_dates=True)
+print(eth)
 
-prices = pd.DataFrame(index=btc.index)
-prices.head()
-prices['Bitcoin'] = btc['Close']
-prices['BNB'] = bnb['Close']
-prices['XRP'] = xrp['Close']
+# prices = pd.DataFrame(index=btc.index)
+# prices.head()
+# prices['Bitcoin'] = btc['Close']
+# prices['BNB'] = bnb['Close']
+# prices['XRP'] = xrp['Close']
+# prices.ffill()
+# print(prices.fillna(0))
+
+prices = pd.concat(
+    [btc['Close'].rename('Bitcoin'),
+     bnb['Close'].rename('BNB'),
+     eth['Close'].rename('Ethereum')],
+    axis=1, join='outer'
+)
 prices.ffill()
 print(prices.fillna(0))
-
-prices.plot()
+# prices.plot()
+prices.loc["2022-05-01":"2022-08-20"].plot()
 plt.show()
 
 
